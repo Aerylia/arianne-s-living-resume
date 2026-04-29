97b24696c64733b2d9067f980defabc1e1afcf28
Aerylia <ariannemeijer@gmail.com>
2026-04-30 00:08:13 +0300

    add react plugin

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,9 @@
 //     error logger plugins, and sandbox detection (port/host/strictPort).
 // You can pass additional config via defineConfig({ vite: { ... } }) if needed.
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/arianne-s-living-resume/', // Important for GitHub Pages!
+  plugins: [react()],
 });
